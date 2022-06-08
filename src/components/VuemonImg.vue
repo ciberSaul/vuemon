ff9981bda4fdecdd96de92eb3c85f98d96724ae9
saul <josesaulmorant@gmail.com>
2022-06-08 19:43:05 +0200

    Añadido los estilos

--- src/components/VuemonImg.vue
+++ src/components/VuemonImg.vue
@@ -1,10 +1,27 @@
 <template>
   <h2>Soy el VuemonImagenes</h2>
+  <!-- https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg -->
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
+.vuokemon-container {
+  height: 200px;
+}
+
+img {
+  height: 200px;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+
+.hidden-vuokemon {
+  filter: brightness(0);
+}
 </style>
